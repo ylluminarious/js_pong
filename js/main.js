3d67e23906809c0e850bb770d939fc5b24ac286a
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-06-09 11:36:00 -0400

    Making ball and paddles global

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 require(["ball_class", "paddle_class", "opening", "buttons", "constants", "game_methods"], function (Ball, Paddle, opening, buttons, constants, GameMethods) {
   var ball = new Ball(constants.COLOR);
   
+  var rightPaddle = new Paddle(
+    constants.RIGHT_PADDLE_X_POS,
+    constants.RIGHT_PADDLE_Y_POS,
+    constants.RIGHT_HORIZONTAL_SCORE_POS,
+    constants.RIGHT_VERTICAL_SCORE_POS,
+    constants.COLOR
+  );
   
+  var leftPaddle = new Paddle(
+    constants.LEFT_PADDLE_X_POS,
+    constants.LEFT_PADDLE_Y_POS,
+    constants.LEFT_HORIZONTAL_SCORE_POS,
+    constants.LEFT_VERTICAL_SCORE_POS,
+    constants.COLOR
+  );
   
   var onePlayer = new GameMethods(ball, rightPaddle, leftPaddle, constants.IS_AI);
   var twoPlayer = new GameMethods(ball, rightPaddle, leftPaddle, constants.IS_NOT_AI);
@@ -11,13 +25,10 @@ require(["ball_class", "paddle_class", "opening", "buttons", "constants", "game_
     input = input || window.event;
     var key_code = input.keyCode;
     if (key_code === constants.ONE_CODE || key_code === constants.ONE_NUMPAD_CODE) {
-      onePlayerGame();
       onePlayer.onePlayerGame();
     }
     if (key_code === constants.TWO_CODE || key_code === constants.TWO_NUMPAD_CODE) {
-      twoPlayerGame();
       twoPlayer.twoPlayerGame();
     }
   };
-  
 });
