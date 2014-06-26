083098a74a28a8c8dec673055af0ecefd94a57ce
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-06-26 17:41:24 -0400

    made the opening scene its own sort of game thing

--- js/opening.js
+++ js/opening.js
@@ -1,26 +1,38 @@
-define(["paddle_class", "ball_class", "game_methods", "constants"], function (Paddle, Ball, GameMethods, constants) {
-  // Ball instance before the game.
-  var ballBeforeGame = new Ball(constants.COLOR_PRE_GAME);
-    
-  // Right paddle instance before the game.
-  var rightPaddleBeforeGame = new Paddle(
-    constants.RIGHT_PADDLE_X_POS,
-    constants.RIGHT_PADDLE_Y_POS,
-    constants.RIGHT_HORIZONTAL_SCORE_POS,
-    constants.RIGHT_VERTICAL_SCORE_POS,
-    constants.COLOR_PRE_GAME
-  );
-  
-  // Left paddle instance before the game.
-  var leftPaddleBeforeGame = new Paddle(
-    constants.LEFT_PADDLE_X_POS,
-    constants.LEFT_PADDLE_Y_POS,
-    constants.LEFT_HORIZONTAL_SCORE_POS,
-    constants.LEFT_VERTICAL_SCORE_POS,
-    constants.COLOR_PRE_GAME
-  );
-  
-  var opening = new GameMethods(ballBeforeGame, rightPaddleBeforeGame, leftPaddleBeforeGame);
-  opening.draw();
-  opening.writeText();
+define(["input_handler", "game_methods", "global_constants", "global_variables"], function (InputHandler, GameMethods, gameConstants, gameVariables) {
+    var OpeningScene = function (ball, rightPaddle, leftPaddle) {
+        var gameMethods = new GameMethods(ball, rightPaddle, leftPaddle);
+        this.draw = function () {
+            gameMethods.draw();
+            gameMethods.writeText();
+        };
+        this.update = function () {
+            ball.updatePosition(rightPaddle, leftPaddle);
+            rightPaddle.AIupdatePosition(ball);
+            leftPaddle.AIupdatePosition(ball);
+        };
+        this.tick = function () {
+            if (gameVariables.whichGame === null) {
+                this.update();
+                this.draw();
+            }
+        };
+        if (gameVariables.whichGame === null) {
+            onkeydown = function () {
+                var event = new InputHandler(ball, rightPaddle, leftPaddle);
+                event.openingSceneHandler();
+                console.log("still in opening scene handler");
+            };
+        }
+        if (gameVariables.whichGame === gameConstants.IS_AI) {
+            onkeydown = function () {
+                var event = new InputHandler(ball, rightPaddle, leftPaddle);
+                event.onePlayerKeyDown();
+            };
+            onkeyup = function () {
+                var event = new InputHandler(ball, rightPaddle, leftPaddle);
+                event.onePlayerKeyUp();
+            };
+        }
+    };
+    return OpeningScene;
 });
