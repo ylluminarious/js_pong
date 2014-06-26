f86deac5f6755bb423b18ee3a965229742132aeb
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-06-26 17:47:59 -0400

    fixed up game_methods

--- js/game_methods.js
+++ js/game_methods.js
@@ -1,61 +1,46 @@
-define(["constants", "one_player_class"], function (constants, OnePlayerGame) {
-  var GameMethods = function (ball, rightPaddle, leftPaddle, isAI) {
-    this.ball = ball; 
-    this.rightPaddle = rightPaddle;
-    this.leftPaddle = leftPaddle;
-    this.draw = function () {
-      constants.CONTEXT.clearRect(constants.ORIGIN, constants.ORIGIN, constants.FIELD.width, constants.FIELD.height);
-      ball.draw();
-      rightPaddle.draw();
-      leftPaddle.draw();
+define(["global_constants", "global_variables", "input_handler"], function (gameConstants, gameVariables, InputHandler) {
+    var GameMethods = function (ball, rightPaddle, leftPaddle) {
+        this.draw = function () {
+            gameConstants.CONTEXT.clearRect(gameConstants.ORIGIN, gameConstants.ORIGIN, gameConstants.FIELD.width, gameConstants.FIELD.height);
+            ball.draw();
+            rightPaddle.draw();
+            leftPaddle.draw();
+        };
+        this.update = function () {
+            ball.updatePosition(rightPaddle, leftPaddle);
+            rightPaddle.updatePosition();
+            if (gameVariables.whichGame === gameConstants.IS_AI) {
+                leftPaddle.AIupdatePosition(ball);
+            } else if (gameVariables.whichGame === gameConstants.IS_NOT_AI) {
+                leftPaddle.updatePosition();
+            }
+        };
+        this.writeText = function () {
+            gameConstants.CONTEXT.fillStyle = gameConstants.COLOR;
+            gameConstants.CONTEXT.font = gameConstants.TEXT_FONT;
+            gameConstants.CONTEXT.fillText("Press \"1\" for single player",
+            gameConstants.LEFT_BUTTON_INSTRUCTIONS_X_POS,
+            gameConstants.LEFT_BUTTON_INSTRUCTIONS_Y_POS
+            );
+            gameConstants.CONTEXT.fillText("Press \"2\" for double player",
+            gameConstants.RIGHT_BUTTON_INSTRUCTIONS_X_POS,
+            gameConstants.RIGHT_BUTTON_INSTRUCTIONS_Y_POS
+            );
+        };
+        this.victory = function (intervalToClear) {
+            this.writeText();
+            clearInterval(intervalToClear);
+            onkeydown = function () {
+                var event = new InputHandler(ball, rightPaddle, leftPaddle);
+                event.victorySceneHandler();
+            };
+        };
+        this.tick = function (intervalToClear) {
+            if (!gameVariables.paused && gameVariables.whichGame !== null) {
+                this.update();
+                this.draw();
+            }
+        };
     };
-    this.update = function () {
-      ball.updatePosition(rightPaddle, leftPaddle);
-      rightPaddle.updatePosition();
-      if (isAI) {
-        leftPaddle.AIupdatePosition(ball);
-      } else {
-        leftPaddle.updatePosition();
-      }
-    };
-    this.writeText = function () {
-      constants.CONTEXT.fillStyle = constants.COLOR;
-      constants.CONTEXT.font = constants.TEXT_FONT;
-      constants.CONTEXT.fillText("Press \"1\" for single player",
-      constants.LEFT_BUTTON_INSTRUCTIONS_X_POS,
-      constants.LEFT_BUTTON_INSTRUCTIONS_Y_POS
-    );
-      constants.CONTEXT.fillText("Press \"2\" for double player",
-      constants.RIGHT_BUTTON_INSTRUCTIONS_X_POS,
-      constants.RIGHT_BUTTON_INSTRUCTIONS_Y_POS
-    );
-    };
-    this.victory = function (loopToClear) {
-      this.writeText();
-      clearInterval(loopToClear);
-      onkeydown = function (input) {
-        // IE code
-        input = input || window.event;
-        var key_code = input.keyCode;
-        input.preventDefault();
-        if (key_code === constants.ONE_CODE || key_code === constants.ONE_NUMPAD_CODE) {
-          var newGame = new OnePlayerGame(ball, rightPaddle, leftPaddle);
-          newGame.loop();
-        }
-        if (key_code === constants.TWO_CODE || key_code === constants.TWO_NUMPAD_CODE) {
-          this.twoPlayerGame();
-        }
-      };
-    };
-    this.tick = function (loopToClear) {
-      if (!constants.PAUSED) {
-        this.draw();
-        this.update();
-        if (rightPaddle.score === 1 || leftPaddle.score === 1) {
-          this.victory(loopToClear);
-        }
-      }
-    };
-  };
-  return GameMethods;
+    return GameMethods;
 });
