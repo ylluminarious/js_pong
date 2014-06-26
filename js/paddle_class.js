76838387915e5670ade469f57accfef39efbd610
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-06-26 17:46:05 -0400

    made a few things nicer in the paddle class

--- js/paddle_class.js
+++ js/paddle_class.js
@@ -1,57 +1,58 @@
-define(["constants"], function (constants) {
-  return function (xPos, yPos, horizontalScorePos, verticalScorePos, color) {
-    this.x = xPos;
-    this.y = yPos;
-    this.horizontalScorePos = horizontalScorePos;
-    this.verticalScorePos = verticalScorePos;
-    this.width = constants.PADDLE_WIDTH;
-    this.height = constants.PADDLE_HEIGHT;
-    this.velocity = constants.STOPPED;
-    this.color = color;
-    this.score = constants.SCORE;
-    this.draw = function () {
-      constants.CONTEXT.fillStyle = this.color;
-      constants.CONTEXT.fillRect(this.x, this.y, this.width, this.height);
-      constants.CONTEXT.font = "50px Courier";
-      constants.CONTEXT.fillText(this.score, this.horizontalScorePos, this.verticalScorePos);
+define(["global_constants", "global_variables"], function (gameConstants, gameVariables) {
+    var Paddle = function (xPos, yPos, horizontalScorePos, verticalScorePos, color) {
+        this.x = xPos;
+        this.y = yPos;
+        this.horizontalScorePos = horizontalScorePos;
+        this.verticalScorePos = verticalScorePos;
+        this.width = gameConstants.PADDLE_WIDTH;
+        this.height = gameConstants.PADDLE_HEIGHT;
+        this.velocity = gameConstants.STOPPED;
+        this.color = color;
+        this.score = gameVariables.score;
+        this.draw = function () {
+            gameConstants.CONTEXT.fillStyle = this.color;
+            gameConstants.CONTEXT.fillRect(this.x, this.y, this.width, this.height);
+            gameConstants.CONTEXT.font = gameConstants.SCORE_FONT;
+            gameConstants.CONTEXT.fillText(this.score, this.horizontalScorePos, this.verticalScorePos);
+        };
+        this.updatePosition = function () {
+            this.y += this.velocity / gameConstants.FPS;
+            if ( this.y < gameConstants.TOP_WALL || (this.y + this.height) > gameConstants.BOTTOM_WALL ) {
+                this.velocity = gameConstants.STOPPED;
+            }
+        };
+        this.AIupdatePosition = function (ball) {
+            // Update position.
+            this.y += this.velocity / gameConstants.FPS;
+            
+            // -------------------- Start of AI code --------------------
+            
+            // If the ball's center is above the paddle's center...
+            if ( ball.y < (this.y + this.height / 2) ) {
+                // ... make the paddle go upwards.
+                this.velocity = -gameConstants.LEFT_PADDLE_VELOCITY;
+            }
+            
+            // If the ball's center is below the paddle's center...
+            if ( ball.y > (this.y + this.height / 2) ) {
+                // ...make the paddle's go downwards.
+                this.velocity = gameConstants.LEFT_PADDLE_VELOCITY;
+            }
+            
+            // If the paddle is touching the top wall...
+            if ( this.y < gameConstants.TOP_WALL && ball.y < (this.y + this.height / 2) ) {
+                // ...stop the paddle.
+                this.velocity = gameConstants.STOPPED;
+            }
+            
+            // If the paddle is touching the bottom wall...
+            if ( (this.y + this.height) > gameConstants.BOTTOM_WALL && ball.y > (this.y + this.height / 2) ) {
+                // ...stop the paddle.
+                this.velocity = gameConstants.STOPPED;
+            }
+            
+            // ******************** End of AI code ********************
+        };
     };
-    this.updatePosition = function () {
-      this.y += this.velocity / constants.FPS;
-      if ( this.y < constants.TOP_WALL || (this.y + this.height) > constants.BOTTOM_WALL ) {
-        this.velocity = constants.STOPPED;
-      }
-    };
-    this.AIupdatePosition = function (ball) {
-      // Update position.
-      this.y += this.velocity / constants.FPS;
- 
-      // -------------------- Start of AI code --------------------
-    
-      // If the ball's center is above the paddle's center...
-      if ( ball.y < (this.y + this.height / 2) ) {
-        // ... make the paddle go upwards.
-        this.velocity = -constants.LEFT_PADDLE_VELOCITY;
-      }
- 
-      // If the ball's center is below the paddle's center...
-      if ( ball.y > (this.y + this.height / 2) ) {
-        // ...make the paddle's go downwards.
-        this.velocity = constants.LEFT_PADDLE_VELOCITY;
-      }
- 
-      // If the paddle is touching the top wall...
-      if ( this.y < constants.TOP_WALL && ball.y < (this.y + this.height / 2) ) {
-        // ...stop the paddle.
-        this.velocity = constants.STOPPED;
-      }
- 
-      // If the paddle is touching the bottom wall...
-      if ( (this.y + this.height) > constants.BOTTOM_WALL && ball.y > (this.y + this.height / 2) ) {
-        // ...stop the paddle.
-        this.velocity = constants.STOPPED;
-      }
-    
-      // ******************** End of AI code ********************
-    };
-  }
+    return Paddle;
 });
