78f10dd488e64440ff039cb35063bc9a38d09656
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-06-30 12:59:25 -0400

    added color variable to main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,24 @@
 require(["ball_class", "paddle_class", "opening", "buttons", "global_constants", "global_variables", "game_methods", "input_handler"], function (Ball, Paddle, OpeningScene, buttons, gameConstants, gameVariables, GameMethods, InputHandler) {
-    // Opening scene objects
-    var openingBall = new Ball(gameConstants.COLOR_PRE_GAME);
-    var openingRightPaddle = new Paddle(
-        gameConstants.RIGHT_PADDLE_X_POS,
-        gameConstants.RIGHT_PADDLE_Y_POS,
-        gameConstants.RIGHT_HORIZONTAL_SCORE_POS,
-        gameConstants.RIGHT_VERTICAL_SCORE_POS,
-        gameConstants.COLOR_PRE_GAME
-    );
-    var openingLeftPaddle = new Paddle(
-        gameConstants.LEFT_PADDLE_X_POS,
-        gameConstants.LEFT_PADDLE_Y_POS,
-        gameConstants.LEFT_HORIZONTAL_SCORE_POS,
-        gameConstants.LEFT_VERTICAL_SCORE_POS,
-        gameConstants.COLOR_PRE_GAME
-    );
-    
     // Game objects
-    var ball = new Ball(gameConstants.COLOR);
+    var ball = new Ball(gameVariables.color);
     var rightPaddle = new Paddle(
         gameConstants.RIGHT_PADDLE_X_POS,
         gameConstants.RIGHT_PADDLE_Y_POS,
         gameConstants.RIGHT_HORIZONTAL_SCORE_POS,
         gameConstants.RIGHT_VERTICAL_SCORE_POS,
-        gameConstants.COLOR
+        gameVariables.color
     );
     var leftPaddle = new Paddle(
         gameConstants.LEFT_PADDLE_X_POS,
         gameConstants.LEFT_PADDLE_Y_POS,
         gameConstants.LEFT_HORIZONTAL_SCORE_POS,
         gameConstants.LEFT_VERTICAL_SCORE_POS,
-        gameConstants.COLOR
+        gameVariables.color
     );
     
     var gameMethods = new GameMethods(ball, rightPaddle, leftPaddle);
-    var event = new InputHandler(ball, rightPaddle, leftPaddle);
-    var opening = new OpeningScene(openingBall, openingRightPaddle, openingLeftPaddle);
+    var event = new InputHandler(ball, rightPaddle, leftPaddle, gameInterval);
+    var opening = new OpeningScene(ball, rightPaddle, leftPaddle);
     var gameInterval = setInterval(gameTick, gameConstants.MILLESECONDS / gameConstants.FPS);
     onkeydown = function () {
         event.keyDown();
@@ -46,8 +29,10 @@ require(["ball_class", "paddle_class", "opening", "buttons", "global_constants",
     function gameTick () {
         if (gameVariables.whichGame === null) {
             opening.tick();
+            gameVariables.color = "gray";
         } else {
             gameMethods.tick();
+            gameVariables.color = "white";
         }
     }
 });
