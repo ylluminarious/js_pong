2a7c767e0f70acc315bf04a4c7d9504639a380ce
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-03 02:25:03 -0400

    fixed AI jittery bug; made AI only run when ball is moving

--- js/paddle_class.js
+++ js/paddle_class.js
@@ -21,33 +21,37 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
             }
         };
         this.AIupdatePosition = function (ball) {
-            // Update position.
-            this.y += this.velocity / gameConstants.FPS;
             
             // -------------------- Start of AI code --------------------
             
-            // If the ball's center is above the paddle's center...
-            if ( ball.y < (this.y + this.height / 2) ) {
-                // ... make the paddle go upwards.
-                this.velocity = -gameConstants.LEFT_PADDLE_VELOCITY;
-            }
+            // If the ball is not stopped, update position and look through the code for the AI.
+            if (ball.horizontalVelocity !== gameConstants.STOPPED && ball.verticalVelocity !== gameConstants.STOPPED) {
+                // Update position.
+                this.y += this.velocity / gameConstants.FPS;
+                
+                // If the ball's center is above the paddle's center...
+                if ( ball.y < (this.y + this.height / 2) ) {
+                    // ... make the paddle go upwards.
+                    this.velocity = -gameConstants.LEFT_PADDLE_VELOCITY;
+                }
+                
+                // If the ball's center is below the paddle's center...
+                if ( ball.y > (this.y + this.height / 2) ) {
+                    // ...make the paddle's go downwards.
+                    this.velocity = gameConstants.LEFT_PADDLE_VELOCITY;
+                }
             
-            // If the ball's center is below the paddle's center...
-            if ( ball.y > (this.y + this.height / 2) ) {
-                // ...make the paddle's go downwards.
-                this.velocity = gameConstants.LEFT_PADDLE_VELOCITY;
-            }
+                // If the paddle is touching the top wall and the ball's center is above the paddle's center (so that the paddle will not stay stopped once the ball goes below it)...
+                if ( this.y < gameConstants.TOP_WALL && ball.y < (this.y + this.height / 2) ) {
+                    // ... stop the paddle.
+                    this.velocity = gameConstants.STOPPED;
+                }
             
-            // If the paddle is touching the top wall and the ball's center is above the paddle's center (so that the paddle will not stay stopped once the ball goes below it)...
-            if ( this.y < gameConstants.TOP_WALL && ball.y < (this.y + this.height / 2) ) {
-                // ... stop the paddle.
-                this.velocity = gameConstants.STOPPED;
-            }
-            
-            // If the paddle is touching the bottom wall and the ball's center is aboce the paddle's center...
-            if ( (this.y + this.height) > gameConstants.BOTTOM_WALL && ball.y > (this.y + this.height / 2) ) {
-                // ... stop the paddle.
-                this.velocity = gameConstants.STOPPED;
+                // If the paddle is touching the bottom wall and the ball's center is aboce the paddle's center...
+                if ( (this.y + this.height) > gameConstants.BOTTOM_WALL && ball.y > (this.y + this.height / 2) ) {
+                    // ... stop the paddle.
+                    this.velocity = gameConstants.STOPPED;
+                }
             }
             
             // ******************** End of AI code ********************
