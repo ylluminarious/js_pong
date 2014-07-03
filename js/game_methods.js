27eda9cdc8ac7f7b9c0bf2448c317510e5a8c3c6
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-03 02:22:56 -0400

    set the ball to center of field when in victory state

--- js/game_methods.js
+++ js/game_methods.js
@@ -16,6 +16,8 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
             rightPaddle.updatePosition();
             if (rightPaddle.score === gameConstants.POINTS_TO_WIN || leftPaddle.score === gameConstants.POINTS_TO_WIN) {
                 gameVariables.whichGame = "victory scene";
+                ball.x = gameConstants.HORIZONTAL_CENTER_OF_FIELD;
+                ball.y = gameConstants.VERTICAL_CENTER_OF_FIELD;
                 rightPaddle.x = gameConstants.RIGHT_PADDLE_X_POS;
                 rightPaddle.y = gameConstants.RIGHT_PADDLE_Y_POS;
                 leftPaddle.x = gameConstants.LEFT_PADDLE_X_POS;
