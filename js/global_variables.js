627109e935ec2963d8a8003856b3282199b35c14
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-07 04:29:15 -0400

    added comments in global_variables.js

--- js/global_variables.js
+++ js/global_variables.js
@@ -1,8 +1,12 @@
 define(function () {
     var gameVariables = {
+        // Game is not paused when it starts
         paused: false,
+        // Game starts out in opening scene
         whichGame: "opening scene",
-        color: null,
+        // Starting color is gray
+        color: "gray",
+        // Both players start out with a score of 0
         score: 0
     };
     return gameVariables;
