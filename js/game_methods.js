450a2482301bb48da4dbe33fecc9a4597e98dae5
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-07 04:30:32 -0400

    put clickToggle function at the top of game_methods.js

--- js/game_methods.js
+++ js/game_methods.js
@@ -1,4 +1,18 @@
 define(["global_constants", "global_variables"], function (gameConstants, gameVariables) {
+    // Click toggle method for the play/pause button (standard jQuery no longer has a toggle method for clicking).
+    (function($) {
+        $.fn.clickToggle = function(func1, func2) {
+            var funcs = [func1, func2];
+            this.data('toggleclicked', 0);
+            this.click(function() {
+                var data = $(this).data();
+                var tc = data.toggleclicked;
+                $.proxy(funcs[tc], this)();
+                data.toggleclicked = (tc + 1) % 2;
+            });
+            return this;
+        };
+    }(jQuery));
     var GameMethods = function (ball, rightPaddle, leftPaddle) {
         // Draws the objects of the game, and the text as well, but only in the victory scene.
         this.draw = function () {
