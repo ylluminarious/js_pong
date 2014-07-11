0cbc66cfa415d7d4afbdffe43c1faf57146f8365
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-11 16:21:35 -0400

    reformatted ball collision code and tried to make it more readable

--- js/ball_class.js
+++ js/ball_class.js
@@ -13,77 +13,87 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
         };
         // Update the ball's position depending on velocity. Also do collision detection for the ball.
         this.updatePosition = function (rightPaddle, leftPaddle) {
+            // Update position.
             this.xPos += this.horizontalVelocity / gameConstants.FPS;
             this.yPos += this.verticalVelocity / gameConstants.FPS;
+            
             // -------------------- Start of ball collision code --------------------
             
-            // Top wall collision.
-            if ((this.yPos - gameConstants.BALL_RADIUS) < gameConstants.TOP_WALL) {
-                this.yPos = gameConstants.BALL_RADIUS;
+            // Ball collision constants (these can't go into gameConstants because they use objects unknown to gameConstants, such as this, rightPaddle, leftPaddle, etc.)
+            const topOfBall = this.yPos - gameConstants.BALL_RADIUS;
+            const bottomOfBall = this.yPos + gameConstants.BALL_RADIUS;
+            const rightSideOfBall = this.xPos + gameConstants.BALL_RADIUS;
+            const leftSideOfBall = this.xPos - gameConstants.BALL_RADIUS;
+            const frontSideOfRightPaddle = rightPaddle.xPos;
+            const topOfRightPaddle = rightPaddle.yPos;
+            const bottomOfRightPaddle = rightPaddle.yPos + rightPaddle.height;
+            const frontSideOfLeftPaddle = leftPaddle.xPos + leftPaddle.width;
+            const topOfLeftPaddle = leftPaddle.yPos;
+            const bottomOfLeftPaddle = leftPaddle.yPos + leftPaddle.height;
+            
+            // Top wall collision
+            if (topOfBall < gameConstants.TOP_WALL) {
+                this.yPos = gameConstants.TOP_WALL + gameConstants.BALL_RADIUS;
                 this.verticalVelocity = -this.verticalVelocity;
             }
             
-            // Bottom wall collision.
-            if ((this.yPos + gameConstants.BALL_RADIUS) > gameConstants.BOTTOM_WALL) {
+            // Bottom wall collision
+            if (bottomOfBall > gameConstants.BOTTOM_WALL) {
                 this.yPos = gameConstants.BOTTOM_WALL - gameConstants.BALL_RADIUS;
                 this.verticalVelocity = -this.verticalVelocity;
             }
             
-            // Right paddle collison on the front side of the paddle.
-            if ((this.xPos + gameConstants.BALL_RADIUS) > rightPaddle.xPos) {
-                if (this.yPos > rightPaddle.yPos) {
-                    if (this.yPos < (rightPaddle.yPos + rightPaddle.height)) {
-                        this.xPos = rightPaddle.xPos - gameConstants.BALL_RADIUS;
+            // Right paddle collison (when the ball goes past the front side of the paddle)
+            if (rightSideOfBall > frontSideOfRightPaddle) {
+                // Front side collision
+                if (this.yPos > topOfRightPaddle) {
+                    if (this.yPos < bottomOfRightPaddle) {
+                        this.xPos = frontSideOfRightPaddle - gameConstants.BALL_RADIUS;
                         this.horizontalVelocity = -this.horizontalVelocity;
+                        console.log("test");
                     }
                 }
-            }
-            
-            // Right paddle collision on the top side of the paddle.
-            if ((this.xPos + gameConstants.BALL_RADIUS) > rightPaddle.xPos) {
-                if ((this.yPos + gameConstants.BALL_RADIUS) > rightPaddle.yPos) {
-                    if ((this.yPos + gameConstants.BALL_RADIUS) < (rightPaddle.yPos + rightPaddle.height)) {
-                        this.yPos = rightPaddle.yPos - gameConstants.BALL_RADIUS;
+                
+                // Top side collision
+                if (bottomOfBall > topOfRightPaddle) {
+                    if (bottomOfBall < bottomOfRightPaddle) {
+                        this.yPos = topOfRightPaddle - gameConstants.BALL_RADIUS;
+                        this.verticalVelocity = -this.verticalVelocity;
+                    }
+                }
+                
+                // Bottom side collision
+                if (topOfBall < bottomOfRightPaddle) {
+                    if (topOfBall > topOfRightPaddle) {
+                        this.yPos = bottomOfRightPaddle + gameConstants.BALL_RADIUS;
                         this.verticalVelocity = -this.verticalVelocity;
                     }
                 }
             }
             
-            // Right paddle collision on the bottom side of the paddle.
-            if ((this.xPos + gameConstants.BALL_RADIUS) > rightPaddle.xPos) {
-                if ((this.yPos - gameConstants.BALL_RADIUS) < (rightPaddle.yPos + rightPaddle.height)) {
-                    if ((this.yPos - gameConstants.BALL_RADIUS) > rightPaddle.yPos) {
-                        this.yPos = (rightPaddle.yPos + rightPaddle.height) + gameConstants.BALL_RADIUS;
-                        this.verticalVelocity = -this.verticalVelocity;
-                    }
-                }
-            }
-            
-            // Left paddle collision on the front side of the paddle.
-            if ((this.xPos - gameConstants.BALL_RADIUS) < (leftPaddle.xPos + leftPaddle.width)) {
-                if (this.yPos > leftPaddle.yPos) {
-                    if (this.yPos < (leftPaddle.yPos + leftPaddle.height)) {
-                        this.xPos = (leftPaddle.xPos + leftPaddle.width) + gameConstants.BALL_RADIUS;
+            // Left paddle collision (when the ball goes past the front side of the paddle)
+            if (leftSideOfBall < frontSideOfLeftPaddle) {
+                // Front side collision
+                if (this.yPos > topOfLeftPaddle) {
+                    if (this.yPos < bottomOfLeftPaddle) {
+                        this.xPos = frontSideOfLeftPaddle + gameConstants.BALL_RADIUS;
                         this.horizontalVelocity = -this.horizontalVelocity;
+                        console.log("test");
                     }
                 }
-            }
-            
-            // Left paddle collision on the top side of the paddle.
-            if ((this.xPos - gameConstants.BALL_RADIUS) < (leftPaddle.xPos + leftPaddle.width)) {
-                if ((this.yPos + gameConstants.BALL_RADIUS) > leftPaddle.yPos) {
-                    if ((this.yPos + gameConstants.BALL_RADIUS) < (leftPaddle.yPos + leftPaddle.height)) {
-                        this.yPos = leftPaddle.yPos - gameConstants.BALL_RADIUS;
+                
+                // Top side collision
+                if (bottomOfBall > topOfLeftPaddle) {
+                    if (bottomOfBall < bottomOfLeftPaddle) {
+                        this.yPos = topOfLeftPaddle - gameConstants.BALL_RADIUS;
                         this.verticalVelocity = -this.verticalVelocity;
                     }
                 }
-            }
-            
-            // Left paddle collision on the bottom side of the paddle.
-            if ((this.xPos - gameConstants.BALL_RADIUS) < (leftPaddle.xPos + leftPaddle.width)) {
-                if ((this.yPos - gameConstants.BALL_RADIUS) < (leftPaddle.yPos + leftPaddle.height)) {
-                    if ((this.yPos - gameConstants.BALL_RADIUS) > leftPaddle.yPos) {
-                        this.yPos = (leftPaddle.yPos + leftPaddle.height) + gameConstants.BALL_RADIUS;
+                
+                // Bottom side collision
+                if (topOfBall < bottomOfLeftPaddle) {
+                    if (topOfBall > topOfLeftPaddle) {
+                        this.yPos = bottomOfLeftPaddle + gameConstants.BALL_RADIUS;
                         this.verticalVelocity = -this.verticalVelocity;
                     }
                 }
@@ -94,7 +104,7 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
             // Restart at center when the ball leaves the field and score a point for the paddle that scored.
             
             // When the ball goes past the right paddle...
-            if ((this.xPos - gameConstants.BALL_RADIUS) > gameConstants.RIGHT_WALL) {
+            if (leftSideOfBall > gameConstants.RIGHT_WALL) {
                 // ... restart at the center of the field and mark a point for the left paddle.
                 this.xPos = gameConstants.HORIZONTAL_CENTER_OF_FIELD;
                 this.yPos = gameConstants.VERTICAL_CENTER_OF_FIELD;
@@ -102,7 +112,7 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
             }
             
             // When the ball goes past the left paddle...
-            if ((this.xPos + gameConstants.BALL_RADIUS) < gameConstants.LEFT_WALL) {
+            if (rightSideOfBall < gameConstants.LEFT_WALL) {
                 // ... restart at the center of the field and mark a point for the right paddle.
                 this.xPos = gameConstants.HORIZONTAL_CENTER_OF_FIELD;
                 this.yPos = gameConstants.VERTICAL_CENTER_OF_FIELD;
