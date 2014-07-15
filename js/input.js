78b6922b13fe8a1c57b82025d0a2be82662266c7
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-15 15:32:09 -0400

    changed a variable name

--- js/input.js
+++ js/input.js
@@ -4,19 +4,19 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             // IE code
             inputEvent = inputEvent || window.event;
             // Get the key code of the key that was pressed
-            var key_pressed = inputEvent.keyCode;
+            var keyPressed = inputEvent.keyCode;
             // Prevent the key's default actions
             inputEvent.preventDefault();
             
             // Opening scene input
             if (gameVariables.whichGame === "opening scene") {
                 // If the key pressed is one or one on the numberpad...
-                if (key_pressed === gameConstants.ONE_CODE || key_pressed === gameConstants.ONE_NUMPAD_CODE) {
+                if (keyPressed === gameConstants.ONE_CODE || keyPressed === gameConstants.ONE_NUMPAD_CODE) {
                     // ... set the game mode to one player.
                     gameVariables.whichGame = "one player";
                 }
                 // If the key pressed is two or two on the numberpad...
-                if (key_pressed === gameConstants.TWO_CODE || key_pressed === gameConstants.TWO_NUMPAD_CODE) {
+                if (keyPressed === gameConstants.TWO_CODE || keyPressed === gameConstants.TWO_NUMPAD_CODE) {
                     // ... set the game mode to two player.
                     gameVariables.whichGame = "two player";
                 }
@@ -25,14 +25,14 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             // One-player game keydown input
             if (gameVariables.whichGame === "one player") {
                 // If the key pressed is the spacebar and the ball isn't moving...
-                if (key_pressed === gameConstants.SPACEBAR_CODE && ball.horizontalVelocity === gameConstants.STOPPED && ball.verticalVelocity === gameConstants.STOPPED) {
+                if (keyPressed === gameConstants.SPACEBAR_CODE && ball.horizontalVelocity === gameConstants.STOPPED && ball.verticalVelocity === gameConstants.STOPPED) {
                     // ... make the ball go leftwards and downwards.
                     ball.horizontalVelocity = -gameConstants.BALL_VELOCITY;
                     ball.verticalVelocity = gameConstants.BALL_VELOCITY;
                 }
 
                 // If the key pressed is up arrow, w, a, or ' (single quote) and the paddle is not touching the top wall...
-                if (key_pressed === gameConstants.UP_ARROW_CODE || key_pressed === gameConstants.W_CODE || key_pressed === gameConstants.A_CODE || key_pressed === gameConstants.SINGLE_QUOTE_CODE) {
+                if (keyPressed === gameConstants.UP_ARROW_CODE || keyPressed === gameConstants.W_CODE || keyPressed === gameConstants.A_CODE || keyPressed === gameConstants.SINGLE_QUOTE_CODE) {
                     if (rightPaddle.yPos > gameConstants.TOP_WALL) {
                         // ... make the paddle go upwards.
                         rightPaddle.velocity = -gameConstants.RIGHT_PADDLE_VELOCITY;
@@ -40,7 +40,7 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
                 }
       
                 // If the key pressed is down arrow, s, z, or / (forward slash) and the paddle is not touching the bottom wall...
-                if (key_pressed === gameConstants.DOWN_ARROW_CODE || key_pressed === gameConstants.S_CODE || key_pressed === gameConstants.Z_CODE || key_pressed === gameConstants.FORWARD_SLASH_CODE) {
+                if (keyPressed === gameConstants.DOWN_ARROW_CODE || keyPressed === gameConstants.S_CODE || keyPressed === gameConstants.Z_CODE || keyPressed === gameConstants.FORWARD_SLASH_CODE) {
                     if ( (rightPaddle.yPos + rightPaddle.height) < gameConstants.BOTTOM_WALL ) {
                         // ... make the paddle go downwards.
                         rightPaddle.velocity = gameConstants.RIGHT_PADDLE_VELOCITY;
@@ -51,14 +51,14 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             // Two-player game keydown input
             if (gameVariables.whichGame === "two player") {
                 // If the key pressed is the spacebar and the ball is not moving...
-                if (key_pressed === gameConstants.SPACEBAR_CODE && ball.horizontalVelocity === gameConstants.STOPPED && ball.verticalVelocity === gameConstants.STOPPED) {
+                if (keyPressed === gameConstants.SPACEBAR_CODE && ball.horizontalVelocity === gameConstants.STOPPED && ball.verticalVelocity === gameConstants.STOPPED) {
                     // ... make the ball move leftwards and downwards.
                     ball.horizontalVelocity = -gameConstants.BALL_VELOCITY;
                     ball.verticalVelocity = gameConstants.BALL_VELOCITY;
                 }
 
                 // If the key pressed is a and the left paddle is not touching the top wall...
-                if (key_pressed === gameConstants.A_CODE) {
+                if (keyPressed === gameConstants.A_CODE) {
                     if (leftPaddle.yPos > gameConstants.TOP_WALL) {
                         // ... make the left paddle move upwards.
                         leftPaddle.velocity = -gameConstants.LEFT_PADDLE_VELOCITY;
@@ -66,7 +66,7 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
                 }
       
                 // If the key pressed is z and the left paddle is not touching the bottom wall...
-                if (key_pressed === gameConstants.Z_CODE) {
+                if (keyPressed === gameConstants.Z_CODE) {
                     if ( (leftPaddle.yPos + leftPaddle.height) < gameConstants.BOTTOM_WALL ) {
                         // ... make the left paddle move downwards.
                         leftPaddle.velocity = gameConstants.LEFT_PADDLE_VELOCITY;
@@ -74,7 +74,7 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
                 }
       
                 // If the key pressed is ' (single quote) and the right paddle is not touching the top wall...
-                if (key_pressed === gameConstants.SINGLE_QUOTE_CODE) {
+                if (keyPressed === gameConstants.SINGLE_QUOTE_CODE) {
                     if (rightPaddle.yPos > gameConstants.TOP_WALL) {
                         // ... make the right paddle move upwards.
                         rightPaddle.velocity = -gameConstants.RIGHT_PADDLE_VELOCITY;
@@ -82,7 +82,7 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
                 }
       
                 // If the key pressed is / (forward slash) and the right paddle is not touching the bottom wall...
-                if (key_pressed === gameConstants.FORWARD_SLASH_CODE) {
+                if (keyPressed === gameConstants.FORWARD_SLASH_CODE) {
                     if ( (rightPaddle.yPos + rightPaddle.height) < gameConstants.BOTTOM_WALL ) {
                         // ... make the right paddle move downwards.
                         rightPaddle.velocity = gameConstants.RIGHT_PADDLE_VELOCITY;
@@ -92,14 +92,14 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             
             // Victory scene input
             if (gameVariables.whichGame === "victory scene") {
-                if (key_pressed === gameConstants.ONE_CODE || key_pressed === gameConstants.ONE_NUMPAD_CODE) {
+                if (keyPressed === gameConstants.ONE_CODE || keyPressed === gameConstants.ONE_NUMPAD_CODE) {
                     rightPaddle.score = gameConstants.NO_POINTS;
                     leftPaddle.score = gameConstants.NO_POINTS;
                     ball.horizontalVelocity = gameConstants.STOPPED;
                     ball.verticalVelocity = gameConstants.STOPPED;
                     gameVariables.whichGame = "one player";
                 }
-                if (key_pressed === gameConstants.TWO_CODE || key_pressed === gameConstants.TWO_NUMPAD_CODE) {
+                if (keyPressed === gameConstants.TWO_CODE || keyPressed === gameConstants.TWO_NUMPAD_CODE) {
                     rightPaddle.score = gameConstants.NO_POINTS;
                     leftPaddle.score = gameConstants.NO_POINTS;
                     ball.horizontalVelocity = gameConstants.STOPPED;
@@ -112,17 +112,17 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             // IE code
             inputEvent = inputEvent || window.event;
             // Get the key code of the key that was pressed
-            var key_pressed = inputEvent.keyCode;
+            var keyPressed = inputEvent.keyCode;
             
             // One-player keyup input
             if (gameVariables.whichGame === "one player") {
                 // If any of the keys for going upwards are released...
-                if (key_pressed === gameConstants.UP_ARROW_CODE || key_pressed === gameConstants.W_CODE || key_pressed === gameConstants.A_CODE || key_pressed === gameConstants.SINGLE_QUOTE_CODE) {
+                if (keyPressed === gameConstants.UP_ARROW_CODE || keyPressed === gameConstants.W_CODE || keyPressed === gameConstants.A_CODE || keyPressed === gameConstants.SINGLE_QUOTE_CODE) {
                     // ... stop moving the paddle.
                     rightPaddle.velocity = gameConstants.STOPPED;
                 }
                 // If any of the keys for going downwards are released...
-                if (key_pressed === gameConstants.DOWN_ARROW_CODE || key_pressed === gameConstants.S_CODE || key_pressed === gameConstants.Z_CODE || key_pressed === gameConstants.FORWARD_SLASH_CODE) {
+                if (keyPressed === gameConstants.DOWN_ARROW_CODE || keyPressed === gameConstants.S_CODE || keyPressed === gameConstants.Z_CODE || keyPressed === gameConstants.FORWARD_SLASH_CODE) {
                     // ... stop moving the paddle.
                     rightPaddle.velocity = gameConstants.STOPPED;
                 }
@@ -131,25 +131,25 @@ define(["global_constants", "global_variables", "game_methods"], function (gameC
             // Two-player keyup input
             if (gameVariables.whichGame === "two player") {
                 // If a is released...
-                if (key_pressed === gameConstants.A_CODE) {
+                if (keyPressed === gameConstants.A_CODE) {
                     // ... stop the left paddle.
                     leftPaddle.velocity = gameConstants.STOPPED;
                 }
       
                 // If z is released...
-                if (key_pressed === gameConstants.Z_CODE) {
+                if (keyPressed === gameConstants.Z_CODE) {
                     // ... stop the left paddle.
                     leftPaddle.velocity = gameConstants.STOPPED;
                 }
       
                 // If ' (single quote) is released...
-                if (key_pressed === gameConstants.SINGLE_QUOTE_CODE) {
+                if (keyPressed === gameConstants.SINGLE_QUOTE_CODE) {
                     // ... stop the right paddle.
                     rightPaddle.velocity = gameConstants.STOPPED;
                 }
       
                 // If / (forward slash) is released...
-                if (key_pressed === gameConstants.FORWARD_SLASH_CODE) {
+                if (keyPressed === gameConstants.FORWARD_SLASH_CODE) {
                     // ... stop the right paddle.
                     rightPaddle.velocity = gameConstants.STOPPED;
                 }
