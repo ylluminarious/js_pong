7fd95a525dcf4babb5c4732ca0fcacd44f0eaad9
George D. Plymale II <gdplymale@lehi.dev.orbitalimpact.com>
2014-07-16 16:19:42 -0400

    added AI constants

--- js/paddle_class.js
+++ js/paddle_class.js
@@ -32,6 +32,10 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
         // Method to update the paddle's position when it's an AI.
         this.AIupdatePosition = function () {
             // -------------------- Start of AI code --------------------
+            // AI constants; these can't go into gameConstants because they use the this object, which is unknown to gameConstants.
+            const CENTER_OF_PADDLE = this.yPos + this.height / 2;
+            const BOTTOM_OF_PADDLE = this.yPos + this.height;
+            
             
             // If the ball is not stopped, update position and look through the code for the AI.
             if (ball.horizontalVelocity !== gameConstants.STOPPED && ball.verticalVelocity !== gameConstants.STOPPED) {
@@ -39,25 +43,25 @@ define(["global_constants", "global_variables"], function (gameConstants, gameVa
                 this.yPos += this.velocity / gameConstants.FPS;
                 
                 // If the ball's center is above the paddle's center...
-                if ( ball.yPos < (this.yPos + this.height / 2) ) {
+                if ( ball.yPos < CENTER_OF_PADDLE ) {
                     // ... make the paddle go upwards.
                     this.velocity = -gameConstants.LEFT_PADDLE_VELOCITY;
                 }
                 
                 // If the ball's center is below the paddle's center...
-                if ( ball.yPos > (this.yPos + this.height / 2) ) {
+                if ( ball.yPos > CENTER_OF_PADDLE ) {
                     // ...make the paddle's go downwards.
                     this.velocity = gameConstants.LEFT_PADDLE_VELOCITY;
                 }
             
                 // If the paddle is touching the top wall and the ball's center is above the paddle's center (so that the paddle will not stay stopped once the ball goes below it)...
-                if ( this.yPos < gameConstants.TOP_WALL && ball.yPos < (this.yPos + this.height / 2) ) {
+                if ( this.yPos < gameConstants.TOP_WALL && ball.yPos < CENTER_OF_PADDLE ) {
                     // ... stop the paddle.
                     this.velocity = gameConstants.STOPPED;
                 }
             
                 // If the paddle is touching the bottom wall and the ball's center is above the paddle's center (again, so that the paddle will not stay stopped once the ball goes above it)...
-                if ( (this.yPos + this.height) > gameConstants.BOTTOM_WALL && ball.yPos > (this.yPos + this.height / 2) ) {
+                if ( BOTTOM_OF_PADDLE > gameConstants.BOTTOM_WALL && ball.yPos > CENTER_OF_PADDLE ) {
                     // ... stop the paddle.
                     this.velocity = gameConstants.STOPPED;
                 }
